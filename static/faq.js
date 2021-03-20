42c2d59c7beff769f67d096c7718e2869ab347d0
rlagkrnws30 <64074330+rlagkrnws30@users.noreply.github.com>
2021-03-21 02:59:44 +0900

    210321_faq_search,filter_function+main_video
    210321
    faq page : search,filter_function
    main page : video

--- static/faq.js
+++ static/faq.js
@@ -19,43 +19,85 @@ navbarToggleCancleBtn.onclick = () => {
 //faq search function
 const searchBar = document.getElementById('searchBar')
 console.log(searchBar)
-searchBar.addEventListener('keyup',(e) => {
-    console.log(e.target.value);
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value;
+    const qa = document.getElementsByClassName('question__answer--accordian')
+    console.log(qa)
+    for (i=0 ; i<qa.length; i++){
+        const title = qa[i].childNodes[1].textContent
+        if(title.toLowerCase().indexOf(searchString) != -1){
+            qa[i].style.display = 'block'
+        }else{
+            qa[i].style.display = 'none'
+        }
+        console.log(title)
+    }
 })
 
 //open accordian
 document.querySelectorAll('.question').forEach((question) =>
-    question.addEventListener('click', ()=>{
+    question.addEventListener('click', () => {
+        if (question.parentNode.classList.contains("active")) {
+            question.parentNode.classList.toggle("active")
+        } else {
+            question.parentNode.classList.remove("active")
+            question.parentNode.classList.add("active")
+        }
+    }))
 
-    if(question.parentNode.classList.contains("active")) {
-        question.parentNode.classList.toggle("active")
-    }
-    else {
-        question.parentNode.classList.remove("active")
-        question.parentNode.classList.add("active")
-    }
-}))
-
-//faq category drop down
 const selected = document.querySelector(".faq__category--text-selected")
 const selectedText = document.querySelector(".faq__category--text-selected-text")
 const dropDown = document.querySelector(".faq__category--dropdown")
 const category = document.querySelectorAll(".faq__category--text")
 
+//faq category drop down
 selected.addEventListener("click", () => {
     dropDown.classList.toggle("new");
 })
 
-category.forEach( o => {
-    o.addEventListener("click", ()=>{
+category.forEach(o => {
+    o.addEventListener("click", () => {
         selectedText.innerHTML = o.querySelector("label").innerHTML;
-        o.querySelector("label").classList.add("selected")
+        o.querySelector("label").innerHTML = selectedText.innerHTML
         dropDown.classList.remove("new")
     })
 })
 
-//선택 후 PC환경에서는
-const a = 0
+//faq category box select
+for(let i=0; i<category.length; i++){
+    category[i].addEventListener('click',
+        filterPost.bind(this, category[i]));
+}
+
+function filterPost(item){
+    for(let i=0; i<category.length; i++){
+        category[i].classList.remove('selected')
+    }
+    item.classList.add('selected')
+}
+
+//faq category filter
+const QAs = document.querySelector('#accordian__question')
+const QA = document.querySelectorAll('.question__answer--accordian')
+
+
+dropDown.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if(filter == null){
+        return;
+    }
+    console.log(filter)
+    QA.forEach((qa)=>{
+        console.log(qa.dataset.type)
+        if(filter ==='*' || filter === qa.dataset.type){
+            qa.classList.remove('invisible');
+            qa.style.display = 'block'
+        }else{
+            qa.classList.add('invisible');
+            qa.style.display = 'none'
+        }
+    });
+});
 
 
 
